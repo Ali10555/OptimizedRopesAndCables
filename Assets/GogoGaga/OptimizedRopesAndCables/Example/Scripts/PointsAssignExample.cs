a016846ebeb609c705dc0a3afce7ff71a08b0cc2
Ali Rasheed <alirasheed1234512345@gmail.com>
2024-07-24 18:57:51 +0500

    changed meshes to prefab

--- Assets/GogoGaga/OptimizedRopesAndCables/Example/Scripts/PointsAssignExample.cs
+++ Assets/GogoGaga/OptimizedRopesAndCables/Example/Scripts/PointsAssignExample.cs
@@ -1,45 +1,47 @@
-using GogoGaga.OptimizedRopesAndCables;
 using UnityEngine;
 
-public class PointsAssignExample : MonoBehaviour
+namespace GogoGaga.OptimizedRopesAndCables
 {
-    [SerializeField] private Transform point1;
-    [SerializeField] private Transform point2;
-    [SerializeField] private Rope rope;
-
-    void Update()
+    public class PointsAssignExample : MonoBehaviour
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        [SerializeField] private Transform point1;
+        [SerializeField] private Transform point2;
+        [SerializeField] private Rope rope;
+
+        void Update()
         {
-            //Assigning new end point to the rope with animation
-            rope.SetEndPoint(point1);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            //Assigning new end point to the rope with animation
-            rope.SetEndPoint(point2);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            //Assigning new end point to the rope without animation
-            //The rope will be recalculated immediately
-            rope.SetEndPoint(point1, true);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            //Assigning new end point to the rope without animation
-            //The rope will be recalculated immediately
-            rope.SetEndPoint(point2, true);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            //Removing end point from the rope, the rope will be
-            //recalculated, LineRenderer will be cleared
-            rope.SetEndPoint(null);
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                //Assigning new end point to the rope with animation
+                rope.SetEndPoint(point1);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                //Assigning new end point to the rope with animation
+                rope.SetEndPoint(point2);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                //Assigning new end point to the rope without animation
+                //The rope will be recalculated immediately
+                rope.SetEndPoint(point1, true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                //Assigning new end point to the rope without animation
+                //The rope will be recalculated immediately
+                rope.SetEndPoint(point2, true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                //Removing end point from the rope, the rope will be
+                //recalculated, LineRenderer will be cleared
+                rope.SetEndPoint(null);
+            }
         }
     }
 }
